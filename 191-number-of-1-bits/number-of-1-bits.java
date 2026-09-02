2d923e33f00e929032d6586c4238fceb6f674a8d
Satvik Chaurasia <satvik9994@gmail.com>
2026-09-02 08:35:31 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (11.72%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.java
+++ 191-number-of-1-bits/number-of-1-bits.java
@@ -1,10 +1,13 @@
-class Solution {
+public class Solution {
     public int hammingWeight(int n) {
-        n = n - ((n >>> 1) & 0x55555555);
-        n = (n & 0x33333333) + ((n >>> 2) & 0x33333333);
-        n = (n + (n >>> 4)) & 0x0f0f0f0f;
-        n = n + (n >>> 8);
-        n = n + (n >>> 16);
-        return n & 0x3f;
+        int count = 0;
+
+        while (n != 0) {
+            if ((n & 1) == 1) {
+                count++;
+            }
+            n >>= 1;
+        }
+        return count;
     }
 }
