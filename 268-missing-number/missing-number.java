6b973460364c7caaabf703c103a0e93b3b120e65
Satvik Chaurasia <satvik9994@gmail.com>
2026-09-02 09:40:00 +0530

    Time: 0 ms (100.00%) | Memory: 47.2 MB (66.18%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -4,7 +4,7 @@ class Solution {
         int sum=n*(n+1)/2;
         int numsum=0;
         for (int i=0;i<n;i++){
-            numsum=numsum+nums[i];
+            numsum+=nums[i];
         }
         int ans=sum-numsum;
         return ans;
